8119756f76d3559542f10e6f0ed574d892d72063
Ali Asadpoor <virtualmatador@yahoo.com>
2020-06-01 22:44:20 -0400

    Create window in main screen

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,10 @@
 int main(int argc, char* argv[])
 {
     auto app = Gtk::Application::create("com.shaidin.thinkora");
+    Gdk::Rectangle monitor_rect;
+    Gdk::Screen::get_default()->get_monitor_geometry(0, monitor_rect);
     Window window;
+    window.move(monitor_rect.get_x(), monitor_rect.get_y());
     window.signal_delete_event().connect(
         sigc::mem_fun(&window, &Window::on_delete));
     return app->run(window);
