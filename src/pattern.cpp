0fcb353f2833c0164d6041c939a5919f5965dbeb
Ali Asadpoor <virtualmatador@yahoo.com>
2020-12-16 00:31:50 -0500

    Improve match polylines

--- src/pattern.cpp
+++ src/pattern.cpp
@@ -40,51 +40,94 @@ const std::vector<std::pair<const Character&, std::size_t>>&
 double Pattern::match(
     const std::vector<Point>& points, const Rectangle& frame) const
 {
-    double ratio = 
-        (frame_[1][0] - frame_[0][0]) /
-        (frame_[1][1] - frame_[0][1]) *
-        (frame[1][1] - frame[0][1]) /
-        (frame[1][0] - frame[0][0]);
-    if (ratio < 2.5 && ratio > 0.4)
+    double total_dist = 0;
+    if (points_.size() == 1)
     {
-        std::vector<Point> pts;
-        for (const auto& point : points)
+        if (points.size() != 1)
         {
-            pts.push_back(
-            {
-                (point[0] - frame[0][0]) * (frame_[1][0] - frame_[0][0]) /
-                    (frame[1][0] - frame[0][0]) + frame_[0][0],
-                (point[1] - frame[0][1]) * (frame_[1][1] - frame_[0][1]) /
-                    (frame[1][1] - frame[0][1]) + frame_[0][1],
-            });
+            total_dist = 1.0;
         }
-        return (compare(pts, points_) + compare(points_, pts)) /
-            get_distance_point(frame_[0], frame_[1]) /
-            (pts.size() * points.size());
+    }
+    else if (points.size() ==1)
+    {
+        total_dist = 1.0;
     }
     else
     {
-        return 1.0;
-    }
-}
-
-double Pattern::compare(const std::vector<Point>& points_a,
-    const std::vector<Point>& points_b) const
-{
-    double total_dist = 0;
-    for (const auto& pt_b : points_b)
-    {
-        auto dist_min = std::numeric_limits<double>::max();
-        auto start = points_a.begin();
-        for (auto it = std::next(start); it != points_a.end(); start = it++)
+        double ratio = 
+            (frame_[1][0] - frame_[0][0]) / (frame_[1][1] - frame_[0][1]) *
+            (frame[1][1] - frame[0][1]) / (frame[1][0] - frame[0][0]);
+        if (ratio < 4 && ratio > 0.25)
         {
-            auto dist = get_distance_line(pt_b, {*start, *it});
-            if (dist_min > dist)
+            std::vector<Point> pts;
+            for (const auto& point : points)
             {
-                dist_min = dist;
+                pts.push_back(
+                {
+                    (point[0] - frame[0][0]) * (frame_[1][0] - frame_[0][0]) /
+                        (frame[1][0] - frame[0][0]) + frame_[0][0],
+                    (point[1] - frame[0][1]) * (frame_[1][1] - frame_[0][1]) /
+                        (frame[1][1] - frame[0][1]) + frame_[0][1],
+                });
             }
+            auto s_it = pts.begin();
+            auto p_it = points_.begin();
+            auto p_it_next = std::next(p_it);
+            for (;;)
+            {
+                auto p_angel = get_angle({
+                    (*p_it_next)[0] - (*p_it)[0],
+                    (*p_it_next)[1] - (*p_it)[1]});
+                auto p_it_next_next = std::next(p_it_next);
+                auto p_angle_next = p_angel;
+                if (p_it_next_next != points_.end())
+                {
+                    p_angle_next = get_angle({
+                        (*p_it_next_next)[0] - (*p_it_next)[0],
+                        (*p_it_next_next)[1] - (*p_it_next)[1]});
+                }
+                Point p_v_start = *s_it, p_v_end =
+                {
+                    (*p_it_next)[0] - (*p_it)[0] + (*s_it)[0],
+                    (*p_it_next)[1] - (*p_it)[1] + (*s_it)[1],
+                };
+                total_dist += get_distance_point(*p_it, *s_it);
+                auto s_it_next = std::next(s_it);
+                for (; s_it_next != pts.end(); ++s_it_next)
+                {
+                    auto s_angle = get_angle({
+                        (*s_it_next)[0] - (*s_it)[0],
+                        (*s_it_next)[1] - (*s_it)[1]});
+                    if (std::abs(get_rotation(s_angle, p_angel)) >
+                        std::abs(get_rotation(s_angle, p_angle_next)))
+                    {
+                        break;
+                    }
+                    total_dist += get_distance_line(
+                        *s_it_next, { p_v_start, p_v_end });
+                    s_it = s_it_next;
+                }
+                total_dist += get_distance_point(*p_it_next, *s_it);
+                if (p_it_next_next == points_.end())
+                {
+                    break;
+                }
+                p_it = p_it_next;
+                p_it_next = p_it_next_next;
+            }
+            auto s_it_next = std::next(s_it);
+            for (; s_it_next != pts.end(); ++s_it_next)
+            {
+                total_dist += get_distance_point(*s_it, *s_it_next);
+                s_it = s_it_next;
+            }
+            total_dist /= points_.size();
+            total_dist /= get_distance_point(frame_[0], frame_[1]);
         }
-        total_dist += dist_min;
+        else
+        {
+            total_dist = 1.0;
+        }
     }
     return total_dist;
 }
